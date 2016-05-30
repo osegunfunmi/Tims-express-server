0c7a7cb8a9920b9a84000b6f67dbc707a2116d1b
tabradford <tabradford@liberty.edu>
2016-05-30 11:54:33 -0400

    Initial commit

---
+++ modules/nav/navBar.js
@@ -0,0 +1,12 @@
+module.exports = {
+    webSiteName: "Tim's Tech",
+    title: "Tim's Express app!!",
+    links: [
+        {
+            link: '/',
+            name: 'Home'
+        }, {
+            link: '/users',
+            name: 'Users'
+        }]
+};
